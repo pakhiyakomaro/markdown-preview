fc7d5a3543348bc59a0aa564a4de4e03e288c146
Aayush Kumar <ayushk780@gmail.com>
2020-10-17 19:27:59 +0530

    Completed Markdown Functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+let marked = require("marked")
+
+export class App extends Component {
+  state = {
+    text: "",
+  };
+  updateMarkdown = (e) => {
+    this.setState({ text: e.target.value });
+  };
+  render() {
+    const { text } = this.state;
+    const markdown = marked(text, {breaks:true, gfm:true});
+    return (
+        <div className="container-fluid">
+          <div>
+            <h2 className="text-center m-4" id="title">Convert your text</h2>
+            <div className="row ">
+              <div className="col-6 ">
+                <h2 className="text-center p-2" id="text-area-label">Enter your text:</h2>
+                <textarea className="form-control" id="editor" value={text} onChange={this.updateMarkdown}/>
+              </div>
+              <div className="col-6" id="preview">
+                <h2 className="text-center p-2" id="text-area-label">See the result:</h2>
+                <div className="preview rounded">
+                <div dangerouslySetInnerHTML={{__html: markdown }}/>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
